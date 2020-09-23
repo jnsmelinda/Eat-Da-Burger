8e3080aeb3e1b6145cf0df46bd7f8f2d7ee894ea
Melinda Ivanov <jns.melinda@gmail.com>
2020-09-23 16:55:44 -0700

    adding data to test

--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,7 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ("aaa", false);
 INSERT INTO burgers (burger_name, devoured) VALUES ("bbb", false);
 INSERT INTO burgers (burger_name, devoured) VALUES ("ccc", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("ccc", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("eee", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("fff", true);
