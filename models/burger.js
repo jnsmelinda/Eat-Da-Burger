6e62f2a1d14cdf52e934d6fe26bb0dba4e28f92e
Melinda Ivanov <jns.melinda@gmail.com>
2020-09-23 16:55:19 -0700

    separating groups of data on ui

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,23 @@
 // import orm.js into
 // Also inside , create the code that will call the ORM functions using burger specific input for the ORM.
+const orm = require("../config/orm.js");
+
+const burger = {
+    all: function(cb) {
+        orm.all("burgers", function(res) {
+          cb(res);
+        });
+    },
+    create: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, function(res) {
+          cb(res);
+        });
+    },
+    update: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(res) {
+          cb(res);
+        });
+    },
+}
+
+module.exports = burger;
