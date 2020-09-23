c41f3e54e2e71c0263c03592315392129f2c6c62
Melinda Ivanov <jns.melinda@gmail.com>
2020-09-23 00:49:36 -0700

    adding router

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,14 @@
-// import the following:
-// * Express
-// * `burger.js`
+const express = require("express");
+const burger = require("../models/burger.js");
+
+var router = express.Router();
+
+router.get('/index', function (req, res) {
+    res.render('index');
+});
+
+router.post("/index", function(req, res) {
+    res.render('index');
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,24 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
 const initdb = require("./db/db-init");
 const { connection } = require("./config/connection.js");
+const router = require("./controllers/burgers_controller.js");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use(router);
 
 function start() {
     connection.connect(function (err) {
         if (err) throw err;
-        console.log("connected as id " + connection.threadId);
-        connection.destroy();
+        console.log("Connected as id " + connection.threadId);
+        app.listen(PORT, function() {
+            console.log("Server listening on: http://localhost:" + PORT);
+        });
+        // connection.destroy();
     });
 }
 
