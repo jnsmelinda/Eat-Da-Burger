c5a90fb82bec4d664f55b0b9c2fe78ecbae97993
Melinda Ivanov <jns.melinda@gmail.com>
2020-09-24 16:25:32 -0700

    seeding db

--- db/seed.sql
+++ db/seed.sql
@@ -1,7 +1,4 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("aaa", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("bbb", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("ccc", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken Bacon Ranch Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("CheeseBurger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Jucy Lucy Burger", false);
 
-INSERT INTO burgers (burger_name, devoured) VALUES ("ccc", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("eee", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("fff", true);
