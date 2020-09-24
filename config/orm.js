06f5d9f7670d9007f584fa44d996c726542206b1
Melinda Ivanov <jns.melinda@gmail.com>
2020-09-23 17:18:37 -0700

    refactoring

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,62 @@
-// * `selectAll()`
-// * `insertOne()`
-// * `updateOne()`
+const { connection } = require("./connection.js");
+
+const orm = {
+    all: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+          cb(result);
+        });
+    },
+    // create: function(table, cols, vals, cb) {
+    //     var queryString = "INSERT INTO " + table;
+
+    //     queryString += " (";
+    //     queryString += cols.toString();
+    //     queryString += ") ";
+    //     queryString += "VALUES (";
+    //     queryString += printQuestionMarks(vals.length);
+    //     queryString += ") ";
+
+    //     console.log(queryString);
+
+    //     connection.query(queryString, vals, function(err, result) {
+    //       if (err) {
+    //         throw err;
+    //       }
+
+    //       cb(result);
+    //     });
+    // },
+}
+
+function printQuestionMarks(num) {
+    let arr = [];
+    for (let i = 0; i < num; i++) {
+      arr.push("?");
+    }
+    return arr.toString();
+}
+
+function selectAll(tableInput, cb) {
+    console.log("select");
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+        if (err) {
+        throw err;
+        }
+        cb(result);
+    });
+}
+
+function insertOne() {
+    console.log("insert");
+}
+
+function updateOne() {
+    console.log("update");
+}
+
+module.exports = orm;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,10 +4,23 @@ const burger = require("../models/burger.js");
 var router = express.Router();
 
 router.get('/index', function (req, res) {
-    res.render('index');
+    burger.all(function(data) {
+        var hbsObject = {
+          burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
 });
 
 router.post("/index", function(req, res) {
+    burger.create([
+        "burger_name", "devoured"
+      ], [
+        req.body.name, req.body.sleepy
+      ], function(result) {
+        res.json({ id: result.insertId });
+      });
     res.render('index');
 });
 
