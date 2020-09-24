e8ebf3db62037b3097beea01c6d1058cf3f6e15e
Melinda Ivanov <jns.melinda@gmail.com>
2020-09-24 16:38:20 -0700

    typo

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,3 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken Bacon Ranch Burger", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("CheeseBurger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese Burger", false);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Jucy Lucy Burger", false);
-
