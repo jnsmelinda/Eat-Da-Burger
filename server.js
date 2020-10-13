610a5f96ed7b3a1d2ef3fdccc57ad0332fdbb97e
Melinda Ivanov <jns.melinda@gmail.com>
2020-10-12 20:54:02 -0700

    adding compression

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const exphbs = require("express-handlebars");
 const initdb = require("./db/db-init");
 const {connection} = require("./config/connection.js");
 const router = require("./controllers/burgers_controller.js");
+let compression = require("compression");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.static("public"));
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 app.use(router);
+app.use(compression());
 
 function start() {
     connection.connect(function(err) {
